29cf80c7997f9ba17063def7823fa408a535eac0
onebottlekick <gksqudcks97@gmail.com>
2022-02-19 22:51:30 +0900

    add classes
    - ConvBlockWithoutActivation
    - BottleneckPSP
    - ResidualBlockPSP

--- sementic_segmentation/models.py
+++ sementic_segmentation/models.py
@@ -50,9 +50,9 @@ class PSPNet(nn.Module):
         return (output, output_aux)
     
     
-class ConvBlock(nn.Module):
+class ConvBlockWithActivation(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, bias):
-        super(ConvBlock, self).__init__()
+        super(ConvBlockWithActivation, self).__init__()
         
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
         self.batchnorm = nn.BatchNorm2d(out_channels)
@@ -69,9 +69,9 @@ class FeatureMap_convolution(nn.Module):
     def __init__(self):
         super(FeatureMap_convolution, self).__init__()
         
-        self.conv1 = ConvBlock(3, 64, kernel_size=3, stride=2, padding=1, dilation=1, bias=False)
-        self.conv2 = ConvBlock(64, 64, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
-        self.conv3 = ConvBlock(64, 128, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
+        self.conv1 = ConvBlockWithActivation(3, 64, kernel_size=3, stride=2, padding=1, dilation=1, bias=False)
+        self.conv2 = ConvBlockWithActivation(64, 64, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
+        self.conv3 = ConvBlockWithActivation(64, 128, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
         
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
@@ -81,3 +81,45 @@ class FeatureMap_convolution(nn.Module):
         x = self.conv3(x)
         outputs = self.maxpool(x)
         return outputs
+    
+    
+class ResidualBlockPSP(nn.Sequnetial):
+    def __init__(self, n_blocks, in_channels, mid_channels, out_channels, stride, dilation):
+        super(ResidualBlockPSP, self).__init__()
+        
+        self.add_module('block1', BottleneckPSP(in_channels, mid_channels, out_channels, stride, dilation))
+        
+        for i in range(n_blocks - 1):
+            self.add_module('block' + str(i+2), BottleneckPSP(out_channels, mid_channels, stride, dilation))
+            
+
+class ConvBlockWithoutActivation(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, bias):
+        super(ConvBlockWithoutActivation, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        outputs = self.batchnorm(x)
+        return outputs
+    
+
+class BottleneckPSP(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, stride, dilation):
+        super(BottleneckPSP, self).__init__()
+        
+        self.conv1 = ConvBlockWithActivation(in_channels, mid_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+        self.conv2 = ConvBlockWithActivation(mid_channels, mid_channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation, bias=False)
+        self.conv3 = ConvBlockWithoutActivation(mid_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False)
+        
+        self.conv_residual = ConvBlockWithoutActivation(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, dilation=1, bias=False)
+
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        conv = self.conv3(self.conv2(self.conv1(x)))
+        residual = self.conv_residual(x)
+        return self.relu(conv + residual)
+        
